35fa09f621cb5270b4fa52e1c2d8315dcf004132
orhan <orhantrkmn749@gmail.com>
2025-11-04 12:13:56 +0300

    feat: add routing for About, Skills, Projects, Work, and Contact pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from "./components/Navbar.tsx";
+import About from './components/About.tsx';
+import Skills from './components/Skills.tsx';
+import Projects from './components/Projects.tsx';
+import Work from "./components/Work.tsx"
+import Contact from './components/Contact.tsx';
+
 function App() {
 
   return (
@@ -9,6 +15,11 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/skills' element={<Skills/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/work' element={<Work/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
     </div>
   )
